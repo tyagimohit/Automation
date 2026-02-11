0008eb05bdb2ae12ae82c0e93361a2f09c04d5db
Mohit <mohit21sep@gmail.com>
2026-02-12 00:28:00 +0530

    Add files via upload

---
+++ src/java/com/example/ainotify/readwhatsapp/entity/Image.java
@@ -0,0 +1,12 @@
+package com.example.ainotify.readwhatsapp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Image {
+    private String id;
+    private String mime_type;
+    private String caption;
+}
---
+++ src/java/com/example/ainotify/readwhatsapp/util/InvoiceOCR.java
@@ -0,0 +1,35 @@
+package com.example.ainotify.readwhatsapp.util;
+
+import net.sourceforge.tess4j.Tesseract;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+@Service
+public class InvoiceOCR {
+
+    @Autowired
+    OpenAIVisionClient openAIVisionClient;
+
+    public String imageToBase64(String imagePath) throws Exception {
+        byte[] imageBytes = Files.readAllBytes(Path.of(imagePath));
+        return Base64.getEncoder().encodeToString(imageBytes);
+    }
+
+    public String getTextFromImage(String imageLocation) throws Exception {
+
+        String imagetext = imageToBase64(imageLocation);
+
+        String imagetextfinal = openAIVisionClient.readImage(imagetext);
+
+
+
+        System.out.println("----- Extracted Invoice Text -----");
+        System.out.println(imagetextfinal);
+        return imagetextfinal;
+    }
+}
